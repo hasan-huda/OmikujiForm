0fe9e9c6e1763934c20dc6ced453ad046bfa5302
Hasan Huda <hasanhuda037@gmail.com>
2023-02-07 00:33:23 -0500

    first commit

---
+++ src/test/java/com/hasan/omikuji/OmikujiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasan.omikuji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmikujiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
